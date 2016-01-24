407ee19facb2572578db1a118a03e8dc1fa8a1c7
joeybloggs <Dean.Karn@metricaid.com>
2016-01-23 22:45:10 -0500

    Add Redirect logic

--- router.go
+++ router.go
@@ -1,6 +1,10 @@
 package lars
 
-import "net/url"
+import (
+	"net/http"
+	"net/url"
+	"strings"
+)
 
 type router struct {
 	lars *LARS
@@ -40,6 +44,51 @@ func (r *router) find(context *ctx, method string, path string) {
 
 	if context.handlers == nil {
 		context.params = context.params[0:0]
+
+		if r.lars.FixTrailingSlash {
+
+			// find again all lowercase
+			lc := strings.ToLower(path)
+			if lc != path {
+				findRoute(context, r.tree, method, lc[1:])
+				if context.handlers != nil {
+					r.redirect(context, method, lc)
+					return
+				}
+			}
+
+			context.params = context.params[0:0]
+
+			if lc[len(lc)-1:] == basePath {
+				lc = lc[:len(lc)-1]
+			} else {
+				lc = lc + basePath
+			}
+
+			// find with lowercase + or - sash
+			findRoute(context, r.tree, method, lc[1:])
+			if context.handlers != nil {
+				r.redirect(context, method, lc)
+				return
+			}
+		}
+
+		context.params = context.params[0:0]
 		context.handlers = append(r.lars.RouteGroup.middleware, r.lars.http404...)
 	}
+}
+
+func (r *router) redirect(context *ctx, method, url string) {
+
+	code := http.StatusMovedPermanently
+
+	if method != GET {
+		code = http.StatusTemporaryRedirect
+	}
+
+	fn := func(c Context) {
+		http.Redirect(c.Response(), c.Request(), url, code)
+	}
+
+	context.handlers = append(r.lars.RouteGroup.middleware, fn)
 }
